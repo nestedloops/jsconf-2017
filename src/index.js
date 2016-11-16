47db7d37194c2bb40a6d1e81891bd14c60a3a06e
Jan Monschke <jan.monschke@gmail.com>
2016-11-16 21:49:17 +0100

    use immutability invariance middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { applyMiddleware, createStore } from 'redux';
+import immutableInvariantMiddleware from 'redux-immutable-state-invariant';
 import createLogger from 'redux-logger';
 import App from './App';
 import reducer from './data/reducer';
@@ -11,7 +12,10 @@ import initial from './data/initial';
 const store = createStore(
   reducer,
   initial,
-  applyMiddleware(createLogger())
+  applyMiddleware(
+    immutableInvariantMiddleware(),
+    createLogger({collapsed: true})
+  )
 );
 
 ReactDOM.render(
