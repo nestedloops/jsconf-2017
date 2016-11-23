4b1bbd3094441ca1d7be101268053996be8d2baf
Jan Monschke <jan.monschke@gmail.com>
2016-11-23 01:03:53 +0100

    write colors to Launchpad

--- src/lib/midi.js
+++ src/lib/midi.js
@@ -4,17 +4,25 @@ import {
   addController
 } from '../data/controllers';
 
-export default {
-  store: null,
+const COLOR_CODES = {
+  OFF: 12,
+  RED: 15,
+  AMBER: 63,
+  YELLOW: 62,
+  GREEN: 60
+};
 
+class Midi {
   init(storeObject) {
     this.store = storeObject;
-    midi.watchPortNames(this.updateControllers.bind(this));
-  },
+    midi.watchPortNames(this.setControllers);
+    this.scheduleUpdate();
+  }
 
-  updateControllers(controllers) {
+  setControllers = (controllers) => {
     const { store } = this;
 
+    // TODO: add logic to remove controllers
     controllers.forEach((id) => {
       if (!store.getState().controllers[id]) {
         const controller = midi(id, {});
@@ -22,4 +30,28 @@ export default {
       }
     });
   }
+
+  updateControllers = () => {
+    const { arrangements, controllers } = this.store.getState();
+    const theArrangement = arrangements.arrangement1;
+    const firstController = controllers[Object.keys(controllers).pop()];
+    if (!firstController) { return this.scheduleUpdate();}
+    for (var y = 0; y < 8; y++) {
+      for (var x = 0; x < 8; x++) {
+        const arrangementButton = theArrangement.buttons[y][x];
+        const key = y * 16 + x;
+        const color = arrangementButton ? COLOR_CODES.GREEN : COLOR_CODES.OFF;
+        firstController.write([144, key, color]);
+      }
+    }
+
+    this.scheduleUpdate();
+  }
+
+  scheduleUpdate() {
+    setTimeout(this.updateControllers, 100);
+  }
 }
+
+const instance = new Midi();
+export default instance;
