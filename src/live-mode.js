c94ae920bfdff0113415fefa70faf01502bcd377
Martin Schuhfuss <m.schuhfuss@gmail.com>
2017-04-06 23:55:39 +0200

    add new videoplayer-component

--- src/live-mode.js
+++ src/live-mode.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import VideoPlayer from './video-player.js';
+
 import './live-mode.css';
 
 class LiveMode extends Component {
   render() {
     const {isVisible} = this.props;
     return (
-      <div className={`liveMode ${isVisible ? 'm-visible' : ''}`} />
+      <div className={`liveMode ${isVisible ? 'm-visible' : ''}`}>
+        <VideoPlayer />
+      </div>
     );
   }
 }
