a847fd993d2b34ba2216145f2850392042506bb6
Martin Schuhfuss <m.schuhfuss@gmail.com>
2017-04-07 10:47:54 +0200

    codestyle, minor fixes

--- src/video-player.js
+++ src/video-player.js
@@ -1,104 +1,99 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class VideoPlayer extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {animationLoopRunning: false};
+    this.state = { animationLoopRunning: false };
   }
 
   render() {
     return (
-        <div ref={el => this.container = el} className="videoPlayer" />
+        <div ref={ el => this.container = el } className="videoPlayer" />
     );
   }
 
   componentDidMount() {
-    if (!this.videoCanvas) {
-      this.videoCanvas = document.createElement('canvas');
+    if (!this.textureCanvas) {
+      this.textureCanvas = document.createElement('canvas');
+
       // FIXME: should be configurable via props or something
-      this.videoCanvas.width = this.videoCanvas.height = 512;
+      this.textureCanvas.width = 512;
+      this.textureCanvas.height = 256;
 
       // FIXME: move to stylesheet
-      this.videoCanvas.style = `width: 100vw; height: 100vh;`;
-      this.videoCanvasCtx = this.videoCanvas.getContext('2d');
-
-      this.startAnimationLoop();
+      this.textureCanvas.style = `width: 100vw; height: 100vh;`;
+      this.videoCanvasCtx = this.textureCanvas.getContext('2d');
     }
 
     // FIXME: this is not the canvas that should ultimately render to screen
-    this.container.appendChild(this.videoCanvas);
+    this.container.appendChild(this.textureCanvas);
+    this.startAnimationLoop();
   }
 
   componentWillUnmount() {
     this.stopAnimationLoop();
-    this.videoCanvas.parentNode.removeChild(this.videoCanvas);
+    this.textureCanvas.parentNode.removeChild(this.textureCanvas);
   }
 
   // ---- animationLoop handling
-
   startAnimationLoop() {
-    this.setState({animationLoopRunning: true});
+    if (this.state.animationLoopRunning) {
+      return;
+    }
+
+    this.setState({ animationLoopRunning: true });
     requestAnimationFrame(this.animationLoopTick.bind(this));
   }
 
   stopAnimationLoop() {
-    this.setState({animationLoopRunning: false});
+    this.setState({ animationLoopRunning: false });
   }
 
   animationLoopTick() {
-    if (!this.state.animationLoopRunning) { return; }
+    if (!this.state.animationLoopRunning) {
+      return;
+    }
 
     requestAnimationFrame(this.animationLoopTick.bind(this));
 
-    const activeVideos = this.props.videos
-        .filter(video => video.isPlaying)
-        .map(video => video.videoElement);
+    const activeVideos = this.props.videos;
 
     const n = activeVideos.length;
-    const ctx = this.videoCanvas.getContext('2d');
-    const w = this.videoCanvas.width;
-    const h = this.videoCanvas.height;
+    const ctx = this.textureCanvas.getContext('2d');
+    const w = this.textureCanvas.width;
+    const h = this.textureCanvas.height;
 
     if (n === 0) {
       ctx.clearRect(0, 0, w, h);
     } else {
-      // FIXME: should consider a different implementation :/
       activeVideos.forEach((video, index) => {
         // FIXME: only use a portion of the source-video to prevent distortion
-        ctx.drawImage(video, index * w / n, 0, w / n, h);
+        ctx.drawImage(video.videoElement, index * w / n, 0, w / n, h);
       });
     }
-
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const {clips, scheduler, fileLoader} = state;
+  const { clips, scheduler, fileLoader } = state;
 
   const videos = Object.keys(clips).reduce((videos, id) => {
     const clip = clips[id];
-    if (clip.type === 'video') {
+    const isPlaying = Object.keys(scheduler.playing).includes(clip.id);
+
+    if (isPlaying && clip.type === 'video') {
       videos.push({
         ...clip,
-        videoElement: fileLoader[clip.file],
-        isPlaying: Object.keys(scheduler.playing).includes(clip.id)
+        videoElement: fileLoader[clip.file]
       });
     }
 
     return videos;
   }, []);
 
-  return {...ownProps, videos};
+  return { ...ownProps, videos };
 };
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    // onClick: () => {
-    //   dispatch(setVisibilityFilter(ownProps.foo));
-    // }
-  }
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(VideoPlayer);
+export default connect(mapStateToProps, null)(VideoPlayer);
