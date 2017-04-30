e5fc333d0c468ff43bfae8f9a5157d8a6cc3d531
Martin Schuhfuss <m.schuhfuss@gmail.com>
2017-05-01 00:23:02 +0200

    remove obsolete props

--- src/live-mode.js
+++ src/live-mode.js
@@ -10,15 +10,7 @@ class LiveMode extends Component {
     const {isVisible} = this.props;
     return (
       <div className={`liveMode ${isVisible ? 'm-visible' : ''}`}>
-        <VideoPlayer
-            resolution="160x90"
-            backgroundColor={0x000000}
-            foregroundColor={0xe10079}
-            pointSize={0.25}
-            luminanceMin={0.2}
-            luminanceMax={0.9}
-            r0={0.9}
-        />
+        <VideoPlayer />
         {isVisible && <VideoControlsGui />}
       </div>
     );
