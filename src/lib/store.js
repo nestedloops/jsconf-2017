832b6e0d0056137f62c64ef232b761e6207bed03
Jan Monschke <jan.monschke@gmail.com>
2017-05-01 22:31:02 +0200

    deactivate redux logger

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,15 +1,12 @@
-import { applyMiddleware, createStore } from 'redux';
-import createLogger from 'redux-logger';
+import { /*applyMiddleware,*/ createStore } from 'redux';
+// import createLogger from 'redux-logger';
 import reducer from '../data/reducer';
-
-const filerOutSchedulerMessages = (_, action) => {
-  return !action.type.includes('FLUSH_');
-};
 
 export default createStore(
   reducer,
-  {},
-  applyMiddleware(
-    createLogger({collapsed: true, predicate: filerOutSchedulerMessages})
-  )
+  {}
+  // use for debugging
+  // ,applyMiddleware(
+  //   createLogger({collapsed: true })
+  // )
 );
